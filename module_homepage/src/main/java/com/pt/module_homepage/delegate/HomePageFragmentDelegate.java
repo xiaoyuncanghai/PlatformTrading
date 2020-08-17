7136b8497bb25cd4ab357b312f47a9d3a723e875
xiaoyuncanghai <2501050940@qq.com>
2020-08-17 10:40:47 +0800

    modify release key

--- module_homepage/src/main/java/com/pt/module_homepage/delegate/HomePageFragmentDelegate.java
+++ module_homepage/src/main/java/com/pt/module_homepage/delegate/HomePageFragmentDelegate.java
@@ -1,5 +1,7 @@
 package com.pt.module_homepage.delegate;
 
+import android.os.Bundle;
+
 import com.pt.lib_common.themvp.view.AppDelegate;
 import com.pt.module_homepage.R;
 
@@ -7,5 +9,15 @@ public class HomePageFragmentDelegate extends AppDelegate {
     @Override
     public int getRootLayoutId() {
         return R.layout.fragment_home_page;
+    }
+
+    @Override
+    public void initWidget(Bundle savedInstanceState) {
+        super.initWidget(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+
     }
 }
