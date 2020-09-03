4b0fb9b3481b3048b5e3d3fcc2467b7c8ad4c2f3
xiaoyuncanghai <2501050940@qq.com>
2020-09-03 15:35:05 +0800

    fix bugs

--- lib_common/src/main/java/com/pt/lib_common/util/GifSizeFilter.java
+++ lib_common/src/main/java/com/pt/lib_common/util/GifSizeFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Zhihu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pt.lib_common.util;
 
 import android.content.Context;
@@ -6,19 +21,12 @@ import android.graphics.Point;
 import com.pt.lib_common.R;
 import com.zhihu.matisse.MimeType;
 import com.zhihu.matisse.filter.Filter;
+import com.zhihu.matisse.internal.entity.IncapableCause;
 import com.zhihu.matisse.internal.entity.Item;
-import com.zhihu.matisse.internal.entity.UncapableCause;
 import com.zhihu.matisse.internal.utils.PhotoMetadataUtils;
 
 import java.util.HashSet;
 import java.util.Set;
-
-/**
- * Author: Jeffer on 2018/3/23 17:28.
- * Email: jeffer7150@163.com
- * Description:
- */
-
 
 public class GifSizeFilter extends Filter {
 
@@ -40,13 +48,13 @@ public class GifSizeFilter extends Filter {
     }
 
     @Override
-    public UncapableCause filter(Context context, Item item) {
+    public IncapableCause filter(Context context, Item item) {
         if (!needFiltering(context, item))
             return null;
 
         Point size = PhotoMetadataUtils.getBitmapBound(context.getContentResolver(), item.getContentUri());
         if (size.x < mMinWidth || size.y < mMinHeight || item.size > mMaxSize) {
-            return new UncapableCause(UncapableCause.DIALOG, context.getString(R.string.error_gif, mMinWidth,
+            return new IncapableCause(IncapableCause.DIALOG, context.getString(R.string.error_gif, mMinWidth,
                     String.valueOf(PhotoMetadataUtils.getSizeInMB(mMaxSize))));
         }
         return null;
--- module_mine/src/main/java/com/pt/module_mine/delegate/PublishSaleActDelegate.java
+++ module_mine/src/main/java/com/pt/module_mine/delegate/PublishSaleActDelegate.java
@@ -3,43 +3,28 @@ package com.pt.module_mine.delegate;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.core.content.FileProvider;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.google.android.material.snackbar.Snackbar;
-import com.pt.lib_common.adapter.ItemSelectDialogAdapter;
-import com.pt.lib_common.bean.ItemSelectDialogModel;
-import com.pt.lib_common.constants.Constant;
 import com.pt.lib_common.themvp.view.AppDelegate;
-import com.pt.lib_common.util.DialogHelp;
 import com.pt.lib_common.util.GifSizeFilter;
-import com.pt.lib_common.util.PhotoUtils;
-import com.pt.lib_common.util.Utils;
 import com.pt.module_mine.R;
-import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.pt.module_mine.adpter.UriAdapter;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.xw.repo.XEditText;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
 import com.zhihu.matisse.engine.impl.GlideEngine;
 import com.zhihu.matisse.filter.Filter;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
@@ -52,6 +37,7 @@ public class PublishSaleActDelegate extends AppDelegate {
     private XEditText publish_sale_location;
     private ImageView img_publish_sale_upload;
     private TextView tv_publish_sale_upload;
+    private static final int REQUEST_CODE_CHOOSE = 23;
 
     @Override
     public int getRootLayoutId() {
@@ -72,143 +58,78 @@ public class PublishSaleActDelegate extends AppDelegate {
         publish_sale_location = get(R.id.publish_sale_location);
         tv_publish_sale_upload = get(R.id.tv_publish_sale_upload);
 
+        rcv_publish_sale_image.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new UriAdapter();
+        rcv_publish_sale_image.setAdapter(mAdapter);
+
         img_publish_sale_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //点击拍照或者是从相册库里面选择
-                showImageOpDialog();
+                RxPermissions rxPermissions = new RxPermissions(PublishSaleActDelegate.this.getActivity());
+                rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE).subscribe(
+                        new Observer<Boolean>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+                            }
+
+                            @Override
+                            public void onNext(Boolean aBoolean) {
+                                if (aBoolean) {
+                                    startAction();
+                                } else {
+                                    Toast.makeText(PublishSaleActDelegate.this.getActivity(), R.string.permission_request_denied, Toast.LENGTH_LONG)
+                                            .show();
+                                }
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+
+                            @Override
+                            public void onComplete() {
+
+                            }
+                        }
+                );
             }
         });
     }
 
-    private List<ItemSelectDialogModel> itemSelectDataList = new ArrayList<>();
-    private String[] itemSelectTitle = new String[]{"拍照", "从相册中选择", "取消"};
-    private File fileUri = new File(Utils.getExternalStoragePath() + "/photo.jpg");
-    private File fileCropUri = new File(Utils.getExternalStoragePath() + "/crop_photo.jpg");
-    private Uri imageUri;
-    private Uri cropImageUri;
-
-    private void showImageOpDialog() {
-        DialogHelp.getInstance().makeItemSelectDialog(this.getActivity(), true);
-        RecyclerView rv_select = DialogHelp.getInstance().contentView.findViewById(R.id.rv_select);
-        rv_select.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        rv_select.setItemAnimator(new DefaultItemAnimator());
-        ItemSelectDialogAdapter itemSelectDialogAdapter = new ItemSelectDialogAdapter(itemSelectDataList);
-        itemSelectDialogAdapter.openLoadAnimation(BaseQuickAdapter.ALPHAIN);
-        rv_select.setAdapter(itemSelectDialogAdapter);
-        itemSelectDialogAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                switch (adapter.getItemViewType(position)) {
-                    case ItemSelectDialogModel.TYPE_CANCEL:
-                        DialogHelp.getInstance().dismissDialog();
-                        break;
-                    case ItemSelectDialogModel.TYPE_SELECT:
-                        switch (position) {
-                            case 0:
-                                //拍照
-                                RxPermissions rxPermissions = new RxPermissions(getActivity());
-                                rxPermissions.request(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE)
-                                        .subscribe(new Observer<Boolean>() {
-                                            @Override
-                                            public void onSubscribe(Disposable d) {
-
-                                            }
-
-                                            @Override
-                                            public void onNext(Boolean aBoolean) {
-                                                if (aBoolean) {
-                                                    if (Utils.externalStorageExist()) {
-                                                        imageUri = Uri.fromFile(fileUri);
-                                                        //通过FileProvider创建一个content类型的Uri
-                                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                                            imageUri = FileProvider.getUriForFile(getActivity(),
-                                                                    "com.pt.platformtrading_location", fileUri);
-                                                        }
-                                                        PhotoUtils.takePicture(getActivity(), imageUri, Constant.REQUEST_CODE_CAMERA);
-                                                    } else {
-                                                        Snackbar.make(getRootView() ,"该设备没有SD卡",Snackbar.LENGTH_LONG).show();
-                                                    }
-                                                    /*Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(
-                                                            new File(Environment.getExternalStorageDirectory(), imgName)));
-                                                    startActivityForResult(cameraIntent, CAMERA_RESULT_CODE);*/
-                                                } else {
-                                                    Snackbar.make(getRootView(), R.string.permission_request_denied,Snackbar.LENGTH_LONG).show();
-                                                }
-                                            }
-
-                                            @Override
-                                            public void onError(Throwable e) {
-
-                                            }
-
-                                            @Override
-                                            public void onComplete() {
-                                                DialogHelp.getInstance().dismissDialog();
-                                            }
-                                        });
-                                break;
-                            case 1:
-                                //相册
-                                break;
-                        }
-                        break;
-                }
-            }
-        });
-
-        itemSelectDataList.clear();
-        ItemSelectDialogModel itemSelectDialogModel1 = new ItemSelectDialogModel(1);
-        itemSelectDialogModel1.setItemText(itemSelectTitle[0]);
-        itemSelectDataList.add(itemSelectDialogModel1);
-
-        ItemSelectDialogModel itemSelectDialogModel2 = new ItemSelectDialogModel(1);
-        itemSelectDialogModel2.setItemText(itemSelectTitle[1]);
-        itemSelectDataList.add(itemSelectDialogModel2);
-
-        ItemSelectDialogModel itemSelectDialogModel = new ItemSelectDialogModel(2);
-        itemSelectDialogModel.setItemText(itemSelectTitle[2]);
-        itemSelectDataList.add(itemSelectDialogModel);
-        itemSelectDialogAdapter.notifyDataSetChanged();
+    private void startAction() {
+        Matisse.from(PublishSaleActDelegate.this.getActivity())
+                .choose(MimeType.ofImage(), false)
+                .countable(true)
+                .capture(true)
+                .captureStrategy(
+                        new CaptureStrategy(true, "com.zhihu.matisse.sample.fileprovider", "test"))
+                .maxSelectable(9)
+                .addFilter(new GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
+                .gridExpectedSize(
+                        getActivity().getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                .thumbnailScale(0.85f)
+                .imageEngine(new GlideEngine())
+                .setOnSelectedListener((uriList, pathList) -> {
+                    Log.e("onSelected", "onSelected: pathList=" + pathList);
+                })
+                .showSingleMediaType(true)
+                .originalEnable(true)
+                .maxOriginalSize(10)
+                .autoHideToolbarOnSingleTap(true)
+                .setOnCheckedListener(isChecked -> {
+                    Log.e("isChecked", "onCheck: isChecked=" + isChecked);
+                })
+                .forResult(REQUEST_CODE_CHOOSE);
+        mAdapter.setData(null, null);
     }
 
-    /*private static final int OUTPUT_X = 480;
-    private static final int OUTPUT_Y = 480;
-    public void onActivityResult(int requestCode , int resultCode , Intent intent){
-        if (resultCode == getActivity().RESULT_OK) {
-            switch (requestCode) {
-                //拍照完成回调
-                case Constant.REQUEST_CODE_CAMERA:
-                    cropImageUri = Uri.fromFile(fileCropUri);
-                    PhotoUtils.cropImageUri(getActivity(), imageUri, cropImageUri, 1, 1, OUTPUT_X, OUTPUT_Y, Constant.REQUEST_CODE_CROP);
-                    break;
-                //访问相册完成回调
-                case Constant.REQUEST_CODE_GALLERY:
-                    if (Utils.externalStorageExist()) {
-                        cropImageUri = Uri.fromFile(fileCropUri);
-                        Uri newUri = Uri.parse(PhotoUtils.getPath(getActivity(), Matisse.obtainResult(intent).get(0)));
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            newUri = FileProvider.getUriForFile(this.getActivity(), "com.yuu1.fulisudi", new File(newUri.getPath()));
-                        }
-                        PhotoUtils.cropImageUri(getActivity(), newUri, cropImageUri, 1, 1, OUTPUT_X, OUTPUT_Y, Constant.REQUEST_CODE_CROP);
-                    } else {
-                        Snackbar.make(refreshLayout ,"该设备没有SD卡",Snackbar.LENGTH_LONG).show();
-                    }
-                    break;
-                case Constant.REQUEST_CODE_CROP:
-                    //  裁剪
-
-                    *//*Bitmap bitmap = PhotoUtils.getBitmapFromUri(cropImageUri, this);
-                    if (bitmap != null) {
-                        showImages(bitmap);
-
-                    }*//*
-                    requetsUploadAvatar(fileCropUri);
-                    break;
-                default:
-            }
+    private UriAdapter mAdapter;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == getActivity().RESULT_OK) {
+            mAdapter.setData(Matisse.obtainResult(data), Matisse.obtainPathResult(data));
+            Log.e("OnActivityResult ", String.valueOf(Matisse.obtainOriginalState(data)));
         }
     }
-*/
 }
